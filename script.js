e3aba5ab13dede2a08d3496aea7871cec65d4ed6
Anushka Verma <anushkaverma779@gmail.com>
2023-03-11 23:30:16 +0530

    3 functions completed

---
+++ script.js
@@ -0,0 +1,74 @@
+let sportsDayColours = ["red", "blue", "green", "yellow"];
+
+
+function OpeningCeremony(Race100M) {
+    let score = {
+        red:100,
+        blue:50,
+        green:150,
+        yellow:100
+    }
+    setTimeout(() => {
+        console.log("Let the games begin");
+        Race100M(score, LongJump);
+        console.log(score);
+    }, 1000);
+}
+
+function Race100M(score, LongJump) {
+    setTimeout(() => {
+        let storeRandomNum = [];
+        for (let key of Object.keys(score)) {
+            let randomNum = Math.floor(Math.random() * (15 - 10 + 1) + 10);
+            storeRandomNum.push(randomNum);
+        }
+        let minNumIdx = storeRandomNum.indexOf(Math.min(...storeRandomNum));
+        let secondMinNumIdx = storeRandomNum.indexOf(Math.min.apply(null, storeRandomNum.filter(n => n != Math.min(...storeRandomNum))));
+        let minKey = sportsDayColours[minNumIdx];
+        let SecondMinKey = sportsDayColours[secondMinNumIdx];
+
+        for (let key of Object.keys(score)) {
+            if(key == minKey) {
+                score[key] += 50;
+            }
+            if (key == SecondMinKey) {
+                score[key] += 25;
+            }
+        }
+
+        let maxScore = 0;
+        for (let key of Object.keys(score)) {
+            if(score[key] > maxScore) {
+                maxScore = score[key];
+            }
+        }
+        console.log(score, maxScore);
+        LongJump(score, LongJump);
+    }, 3000);
+}
+
+function LongJump(score, LongJump) {
+    let randomColour = sportsDayColours[Math.floor(Math.random() * (3 - 0 + 1) + 0)];
+    for (let key of Object.keys(score)) {
+        if(key == randomColour) {
+            score[key] += 150;
+        }
+    }
+    let maxScore = 0;
+    for (let key of Object.keys(score)) {
+        if(score[key] > maxScore) {
+            maxScore = score[key];
+        }
+    }
+    console.log(score, maxScore);
+}
+
+function HighJump() {
+
+}
+
+function AwardCeremony() {
+
+}
+
+OpeningCeremony(Race100M);
